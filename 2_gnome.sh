660ea7e57cfb3f89879dd3bfb47b3d4dd1f569f5
exah-io <3874515+exah-io@users.noreply.github.com>
2020-04-25 13:01:26 +0100

    gnome improvements

--- 2_gnome.sh
+++ 2_gnome.sh
@@ -12,11 +12,25 @@ echo "Enabling GDM"
 sudo systemctl enable gdm.service
 
 echo "Enabling automatic login"
-sudo tee -a /etc/lightdm/lightdm.conf << END
+sudo tee -a /etc/gdm/custom.conf << END
 # Enable automatic login for user
 [daemon]
 AutomaticLogin=$USER
 AutomaticLoginEnable=True
 END
+
+echo "Installing themes"
+# Kali themes source: https://gitlab.com/kalilinux/packages/kali-themes/-/tree/kali/master/share/themes
+mkdir -p ~/.themes
+wget -P ~/.themes https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/themes/kali-themes.tar
+tar -xzf ~/.themes/kali-themes.tar -C ~/.themes
+rm -f ~/.themes/kali-themes.tar
+
+echo "Installing icon themes"
+# Kali themes source: https://gitlab.com/kalilinux/packages/kali-themes/-/tree/kali/master/share/icons
+mkdir -p ~/.icons
+wget -P ~/.icons https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/themes/kali-icons.tar
+tar -xzf ~/.icons/kali-icons.tar -C ~/.icons
+rm -f ~/.icons/kali-icons.tar
 
 echo "Your setup is ready. You can reboot now!"
