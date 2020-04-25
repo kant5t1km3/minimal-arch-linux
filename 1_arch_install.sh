da1c9ef405fa7b9140c0a63de7cae538d1140121
exah-io <3874515+exah-io@users.noreply.github.com>
2020-04-25 14:46:48 +0100

    Update 1_arch_install.sh

--- 1_arch_install.sh
+++ 1_arch_install.sh
@@ -15,6 +15,9 @@ timedatectl set-timezone $continent_city
 echo "Syncing packages database"
 pacman -Sy --noconfirm
 
+echo "Verifying master keys"
+pacman-key --populate archlinux
+
 echo "Creating partition tables"
 printf "n\n1\n4096\n+512M\nef00\nw\ny\n" | gdisk /dev/nvme0n1
 printf "n\n2\n\n\n8e00\nw\ny\n" | gdisk /dev/nvme0n1
