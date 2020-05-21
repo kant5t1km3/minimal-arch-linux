54200941262fa188c184855fbe3302540059eefc
exah-io <3874515+exah-io@users.noreply.github.com>
2020-05-21 12:21:32 +0100

    new wallpapers + comment nodejs

--- 2_base.sh
+++ 2_base.sh
@@ -29,9 +29,11 @@ sudo pacman -S --noconfirm ttf-roboto ttf-droid ttf-opensans ttf-dejavu ttf-libe
 echo "Downloading wallpapers"
 wget -P ~/Pictures/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/wallpapers/ahw57vapx9h41.png
 wget -P ~/Pictures/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/wallpapers/snro7b9hso141.png
+wget -P ~/Pictures/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/wallpapers/BU9AoyoEJgMgmmqKsAV7Kjr8PrRGhWiAZNRlbX8MWNw.png
+wget -P ~/Pictures/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/wallpapers/DIGCO6c0q1wn3ueu_Uebpuv7BfKaQJgKhemcH_3vfVQ.png
 
-echo "Installing Node.js LTS"
-sudo pacman -S --noconfirm nodejs-lts-erbium
+# echo "Installing Node.js LTS"
+# sudo pacman -S --noconfirm nodejs-lts-erbium
 
-echo "Increasing the amount of inotify watchers"
-echo fs.inotify.max_user_watches=524288 | sudo tee /etc/sysctl.d/40-max-user-watches.conf && sudo sysctl --system
+# echo "Increasing the amount of inotify watchers"
+# echo fs.inotify.max_user_watches=524288 | sudo tee /etc/sysctl.d/40-max-user-watches.conf && sudo sysctl --system
