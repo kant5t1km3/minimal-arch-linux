7620745bd5f452b5ef9e7bb14cc6ea8e57e033e2
exah-io <3874515+exah-io@users.noreply.github.com>
2020-06-12 14:52:53 +0100

    video acceleration + sway support

--- 2_base.sh
+++ 2_base.sh
@@ -15,7 +15,10 @@ sudo ufw default deny incoming
 sudo ufw default allow outgoing
 
 echo "Installing additional Intel drivers"
-sudo pacman -S --noconfirm intel-media-driver lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader
+sudo pacman -S --noconfirm mesa lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader
+
+echo "Enabling video accelaration "
+sudo pacman -S --noconfirm libva libva-utils libva-intel-driver ffmpeg
 
 # Reference: https://github.com/lutris/docs/blob/master/WineDependencies.md
 # echo "Installing Lutris (with Wine support)
@@ -27,6 +30,7 @@ sudo pacman -S --noconfirm neovim keepassxc git openssh links upower htop powert
 echo "Installing Firefox Flatpak"
 flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak --user --assumeyes install flathub org.mozilla.firefox
+flatpak override --user --env=MOZ_ENABLE_WAYLAND=1 org.mozilla.firefox
 
 echo "Setting automatic updates for Flatpak apps"
 touch ~/.config/systemd/user/flatpak-update.timer
@@ -66,9 +70,19 @@ wget -P ~/Pictures/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux
 wget -P ~/Pictures/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/wallpapers/snro7b9hso141.png
 wget -P ~/Pictures/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/wallpapers/BU9AoyoEJgMgmmqKsAV7Kjr8PrRGhWiAZNRlbX8MWNw.png
 wget -P ~/Pictures/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/wallpapers/DIGCO6c0q1wn3ueu_Uebpuv7BfKaQJgKhemcH_3vfVQ.png
+wget -P ~/Pictures/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/wallpapers/nasa-japanese.jpg
 
 # echo "Installing Node.js LTS"
 # sudo pacman -S --noconfirm nodejs-lts-erbium
 
 # echo "Increasing the amount of inotify watchers"
 # echo fs.inotify.max_user_watches=524288 | sudo tee /etc/sysctl.d/40-max-user-watches.conf && sudo sysctl --system
+
+# echo "Installing zsh"
+# sudo pacman -S --noconfirm zsh zsh-completions
+# chsh -s /usr/bin/zsh
+
+# echo "Installing powerlevel10k theme"
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+# wget -P ~/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/dotfiles/zsh/.p10k.zsh
+# wget -P ~/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/dotfiles/zsh/.zshrc
---
+++ 2_sway.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+echo "Downloading and running base script"
+wget https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/2_base.sh
+chmod +x 2_base.sh
+sh ./2_base.sh
+
+echo "Installing sway"
+sudo pacman -S --noconfirm sway swaylock swayidle waybar rofi light pulseaudio pavucontrol slurp grim
+
+echo "Ricing sway"
+mkdir -p ~/.config/sway
+wget -P ~/.config/sway/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/dotfiles/sway/config
+
+echo "Ricing waybar"
+mkdir -p ~/.config/waybar
+wget -P ~/.config/waybar/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/dotfiles/waybar/config
+
+echo "Ricing rofi"
+mkdir -p ~/.config/rofi/config
+wget -P ~/.config/rofi/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/dotfiles/rofi/base16-one-light.config
+wget -P ~/.config/rofi/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/dotfiles/rofi/base16-one-light.rasi
+wget -P ~/.config/rofi/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/dotfiles/rofi/base16-onedark.config
+wget -P ~/.config/rofi/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/dotfiles/rofi/base16-onedark.rasi
+
+echo "Enabling sway autostart"
+touch ~/.bash_profile
+tee -a ~/.bash_profile << EOF
+# If running from tty1 start sway
+if [ "$(tty)" = "/dev/tty1" ]; then
+	exec sway
+fi
+EOF
+
+echo "Ricing vim"
+wget -P ~/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/dotfiles/vim/.vimrc
+
+echo "Installing and ricing Alacritty terminal"
+sudo pacman -S --noconfirm alacritty
+mkdir -p ~/.config/alacritty/
+wget -P ~/.config/alacritty/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/dotfiles/alacritty/alacritty.yml
+
+echo "Installing thunar with auto-mount and archives creation/deflation support"
+sudo pacman -S --noconfirm thunar gvfs thunar-volman thunar-archive-plugin ark file-roller xarchiver
+
+echo "Installing PDF viewer"
+sudo pacman -s --noconfirm xreader
+
+echo "Installing Thunderbird Flatpak with Wayland support"
+flatpak --user --assumeyes install flathub org.mozilla.Thunderbird
+flatpak override --user --env=MOZ_ENABLE_WAYLAND=1 org.mozilla.Thunderbird
+
+echo "Downloading themes (Kali Linux theme without the dragon)"
+# Kali themes source: https://gitlab.com/kalilinux/packages/kali-themes/-/tree/kali/master/share/themes
+mkdir -p ~/.themes
+wget -P ~/.themes https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/themes/kali-themes.tar.gz
+tar -xzf ~/.themes/kali-themes.tar.gz -C ~/.themes
+rm -f ~/.themes/kali-themes.tar.gz
+
+echo "Downloading icon themes (Kali Linux icons)"
+# Kali themes source: https://gitlab.com/kalilinux/packages/kali-themes/-/tree/kali/master/share/icons
+mkdir -p ~/.icons
+wget -P ~/.icons https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/icons/kali-icons.tar.xz
+tar -xzf ~/.icons/kali-icons.tar.xz -C ~/.icons
+rm -f ~/.icons/kali-icons.tar.xz
+
+echo "Setting GTK theme, font and icons"
+FONT="Fira Code Regular 10"
+GTK_THEME="Kali-Light"
+GTK_ICON_THEME="Flat-Remix-Blue-Dark"
+GTK_SCHEMA="org.gnome.desktop.interface"
+gsettings set $GTK_SCHEMA gtk-theme "$GTK_THEME"
+gsettings set $GTK_SCHEMA icon-theme "$GTK_ICON_THEME"
+gsettings set $GTK_SCHEMA font-name "$FONT"
+gsettings set $GTK_SCHEMA document-font-name "$FONT"
+
+echo "Enabling suspend and hibernate hotkeys"
+sudo sed -i 's/#HandlePowerKey=poweroff/HandlePowerKey=hibernate/g' /etc/systemd/logind.conf
+sudo sed -i 's/#HandleLidSwitch=suspend/HandleLidSwitch=suspend/g' /etc/systemd/logind.conf
+
+echo "Blacklisting bluetooth"
+sudo touch /etc/modprobe.d/nobt.conf
+sudo tee -a /etc/modprobe.d/nobt.conf << END
+blacklist btusb
+blacklist bluetooth
+END
+sudo mkinitcpio -p linux-lts
+sudo mkinitcpio -p linux
+
+echo "Enabling autologin"
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d/
+sudo touch /etc/systemd/system/getty@tty1.service.d/override.conf
+sudo tee -a /etc/systemd/system/getty@tty1.service.d/override.conf << END
+[Service]
+ExecStart=
+ExecStart=-/usr/bin/agetty --skip-login --nonewline --noissue --autologin $USER --noclear %I $TERM
+END
+
+echo "Removing last login message"
+touch ~/.hushlogin
+
+echo "Installing xwayland"
+sudo pacman -S --noconfirm xorg-server-xwayland
